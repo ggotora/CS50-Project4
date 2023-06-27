7508e5d677e0a877eb6eec0dfc2ddf24f6f937c5
Gilbert Gotora <gilbertgotora2@gmail.com>
2023-06-27 20:41:21 +0200

    add Profile model and str methods

--- network/models.py
+++ network/models.py
@@ -5,6 +5,13 @@ from django.db import models
 class User(AbstractUser):
     pass 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField('self', related_name="followed_by", symmetrical=False)
+
+    def __str__(self) -> str:
+        return f"{self.user.username} Profile"
+
 class Comment(models.Model):
     content = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -16,3 +23,6 @@ class Post(models.Model):
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, blank=True, null=True)
     pub_date = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name="post_like", blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.content[:15]} ..."
